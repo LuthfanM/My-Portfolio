3615a3ad9e9b0ac4f30d84f2c5c9a3cff774e02b
AmienulRana <amienulrana@gmail.com>
2023-05-29 08:27:43 +0700

    move text say hello to bottom skills

--- src/app/projects/projects.jsx
+++ src/app/projects/projects.jsx
@@ -27,30 +27,6 @@ export default function Project() {
       <div data-aos="zoom-in" data-aos-duration="600" data-aos-once="true">
         <ListProject />
       </div>
-      <div className="my-60 flex flex-col items-center justify-center gap-3 text-center md:gap-5">
-        <p className="font-mono text-accent" data-aos="fade-up">
-          What{`'`}s Next?
-        </p>
-        <h1
-          className="pb-1 text-4xl font-semibold leading-relaxed text-secondary md:text-5xl"
-          data-aos="zoom-in-up"
-        >
-          Get In Touch
-        </h1>
-        <p
-          className="w-full pb-8 text-sm leading-relaxed text-primary md:w-5/6 md:text-base lg:w-4/6 xl:w-1/2"
-          data-aos="zoom-in-up"
-        >
-        I am currently working and actively taking on freelance projects to expand my experience in the industry. If you have any questions or would like to get in touch, please feel free to contact me. I will do my best to respond promptly.
-        </p>
-        <button
-          className="rounded-[4px] border-2 border-accent px-7 py-4 font-mono text-accent transition-all duration-300 hover:bg-accent hover:bg-opacity-10"
-          data-aos="flip-up"
-          data-aos-duration="600"
-        >
-          <a href="mailto:amienulrana@gmail.com">Say Hello</a>
-        </button>
-      </div>
     </div>
   );
 }
