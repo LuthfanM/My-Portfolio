88a4ecf327e446919502047228ebc017b99e3ae6
Luthfan <mursyidanluthfan@gmail.com>
2026-05-26 12:44:38 +0700

    fix: Remove react schrol

--- src/components/navbar/HorizontalList.tsx
+++ src/components/navbar/HorizontalList.tsx
@@ -1,35 +1,50 @@
-import React, { useRef } from "react";
+"use client";
+
+import React from "react";
 import Link from "next/link";
-import { Scrollchor } from "react-scrollchor";
 import { dataMenus } from "../../app/data/menuList";
 
 const HorizontalList = () => {
+  const handleScroll = (target) => {
+    const id = target.replace("#", "");
+    const element = document.getElementById(id);
+
+    if (element) {
+      element.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+      
+      window.history.pushState(null, "", `#${id}`);
+    }
+  };
+
   return (
     <div className="hidden md:flex space-x-4 text-red-50">
-      {dataMenus &&
-        dataMenus.map((datamenu, idx) => {
-          if (datamenu.type === "page") {
-            return (
-              <Link
-                key={"data-menu" + idx}
-                href={datamenu.link}
-                className="nav-link"
-              >
-                {datamenu.name}
-              </Link>
-            );
-          }
-
+      {dataMenus?.map((datamenu, idx) => {
+        if (datamenu.type === "page") {
           return (
-            <Scrollchor
-              key={"data-menu" + idx}
-              to={datamenu.link}
+            <Link
+              key={`data-menu-${idx}`}
+              href={datamenu.link}
               className="nav-link"
             >
-              <div>{datamenu.name}</div>
-            </Scrollchor>
+              {datamenu.name}
+            </Link>
           );
-        })}
+        }
+
+        return (
+          <button
+            key={`data-menu-${idx}`}
+            type="button"
+            onClick={() => handleScroll(datamenu.link)}
+            className="nav-link"
+          >
+            {datamenu.name}
+          </button>
+        );
+      })}
     </div>
   );
 };
